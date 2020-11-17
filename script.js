1434ec8840879d883fde216048287ff7e798ba34
pjmantoss <joelptoss@gmail.com>
2020-11-17 14:09:34 +0100

    update script

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Variables
 const url = "https://api.punkapi.com/v2/beers?page=";
-
+const filterABV = document.getElementById("filterABV");
+const filterIBU = document.getElementById("filterIBU");
+const pagetText = document.getElementById("pageNumber");
 
 // Filters
