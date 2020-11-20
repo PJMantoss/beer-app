490424eec147927f7ca81eb9655f0c6a82e9fdab
pjmantoss <joelptoss@gmail.com>
2020-11-20 14:27:33 +0100

    update js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const api = "https://api.punkapi.com/v2/beers?page=";
 const filterABV = document.getElementById("filterABV");
 const filterIBU = document.getElementById("filterIBU");
-const pagetText = document.getElementById("pageNumber");
+const pageText = document.getElementById("pageNumber");
 const prevPage = document.getElementById("prevPage");
 const nextPage = document.getElementById("nextPage");
 let optionsABV = "", optionsIBU = "", page = 1;
